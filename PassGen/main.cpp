a8acbaee6fc267db01d861cb5dad2758ed23c8a8
Zero.[0] <94681440+Podorozhnyk-0@users.noreply.github.com>
2022-05-01 12:10:48 +0300

    Update main.cpp

--- PassGen/main.cpp
+++ PassGen/main.cpp
@@ -4,48 +4,17 @@
 
 using namespace std;
 
-bool DebugFunc(wstring psw, int minDigitsCount, int maxDigitsCount, int minUnderlinesCount, int maxUnderlinesCount, int minCapitalLettersCount, int maxCapitalLettersCount)
-{
-	int digs = 0, unders = 0, caps = 0;
-	for (int i = 0; i < psw.size(); i++)
-	{
-		if(iswalpha(psw[i]) && iswupper(psw[i]))
-		{
-			caps++;
-		}
-		if(iswdigit(psw[i]))
-		{
-			digs++;
-		}
-		if(psw[i] == '_')
-		{
-			unders++;
-		}
-	}
-	wcout << digs << " " << unders << " " << caps << endl;
-	bool res = ((digs <= maxDigitsCount && digs >= minDigitsCount) && (unders <= maxUnderlinesCount && unders >= minUnderlinesCount) && (caps <= maxCapitalLettersCount && caps >= minCapitalLettersCount));
-	return res;
-}
-
 int main()
 {
     srand(time(NULL));
 	int c = 200;
-	int co = 0;
 	GenerationSettings g(200, 200000, 200000, 200000, 200000, 200000, 200000);
     while (c)
     {
 		
 		wstring someStr = wgenerate_password(g);
-		bool gt;
-		if(gt = DebugFunc(someStr, g.getMinDigitsCount(), g.getMaxDigitsCount(), g.getMinUnderlinesCount(),
-			g.getMaxUnderlinesCount(), g.getMinCapitalLettersCount(), g.getMaxCapitalLettersCount()))
-		{
-			co++;
-		}
-		wcout << someStr << " " << gt << " " << "digitsMax: " << g.getMaxDigitsCount() << " digitsMin " << g.getMinDigitsCount() << " underMax " << g.getMaxUnderlinesCount() << " underMin " << g.getMinUnderlinesCount()
+		wcout << someStr << " " << "digitsMax: " << g.getMaxDigitsCount() << " digitsMin " << g.getMinDigitsCount() << " underMax " << g.getMaxUnderlinesCount() << " underMin " << g.getMinUnderlinesCount()
 			<< " capitalMax " << g.getMaxCapitalLettersCount() << " capitalMin " << g.getMinCapitalLettersCount() << endl;
 		c--;
     }
-	wcout << co << endl;
 }
